bac5276f5904e8f641ec6200d2c842e86bbab295
MusicManiac <14209235+MusicManiac@users.noreply.github.com>
2024-11-19 13:25:58 +0100

    Fixed static files loading with nginx

--- app/tasks_manager_project/settings.py
+++ app/tasks_manager_project/settings.py
@@ -19,14 +19,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-USE_X_FORWARDED_HOST = True
-USE_X_FORWARDED_PORT = True
+# USE_X_FORWARDED_HOST = True
+# USE_X_FORWARDED_PORT = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-(jxy_wp$mud8r7us6qd%ny9z0zone)3#8o^4k=m9yf(k3h^uxn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '127.0.0.1:8000', '127.0.0.1:1337']
 
@@ -134,9 +134,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = Path(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [Path(BASE_DIR,'static'),]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
